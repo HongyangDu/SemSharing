0ec35138adf0a60606060d21b20036dd53d014d8
hongyang <53124436+HongyangDu@users.noreply.github.com>
2023-06-28 14:20:14 +0800

    upload

--- first_client.py
+++ first_client.py
@@ -1,3 +1,4 @@
+# Import required modules
 from api.ReconAnom import JSRNet_api
 from api.SuperPoint import SuperPoint_api
 import cv2
@@ -5,22 +6,29 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 class first_info():
     def __init__(self):
+        # Instantiate the JSRNet and SuperPoint APIs
         self.jsr = JSRNet_api()
         self.superpoint = SuperPoint_api()
-    def extract_one_image(self, img_path, name='0', size = (768, 768)):
 
+    def extract_one_image(self, img_path, name='0', size=(768, 768)):
+        # Read the image in color (1 in cv2.imread() stands for color)
         query = cv2.imread(img_path, 1)
-        # query_rgb = cv2.imread(query_path, 1)
 
+        # Resize the image to the specified size
         query = cv2.resize(query, size)
 
+        # Prepare inputs for the SuperPoint and JSRNet
+        # Convert the query image to grayscale for SuperPoint and normalize
         sup_inp = cv2.cvtColor(query, cv2.COLOR_BGR2GRAY) / 255.
+        # Use the color image for JSRNet
         jsr_inp = query
 
+        # Run the APIs on the inputs
         jsr_res = self.jsr.run(jsr_inp)
         sup_res = self.superpoint.run_SuperPoint(sup_inp, name)
 
-
+        # Return the results from both APIs
         return jsr_res, sup_res
--- second_client.py
+++ second_client.py
@@ -1,3 +1,4 @@
+# Import necessary libraries
 import matplotlib.pyplot as plt
 import scipy.io as io
 from SuperGlue.models.superglue import SuperGlue
@@ -10,142 +11,136 @@ import torch
 import skimage
 import numpy as np
 import matplotlib.cm as cm
-# os.environ["CUDA_VISIBLE_DEVICES"] = '3'
-# from piq import ssim, SSIMLoss, haarpsi, HaarPSILoss, dss, DSSLoss
 
-
+# Function to blend two images together with a specified blending factor
 def blend(a, b, alpha=0.5):
-    """
-    Alpha blend two images.
-    Parameters
-    ----------
-    a, b : numpy.ndarray
-        Images to blend.
-    alpha : float
-        Blending factor.
-    Returns
-    -------
-    result : numpy.ndarray
-        Blended image.
-    """
-
     a = skimage.img_as_float(a)
     b = skimage.img_as_float(b)
     return a*alpha+(1-alpha)*b
 
 class second_info():
     def __init__(self, weight='outdoor', match_threshold=0.2):
+        # Set SuperGlue parameters
         superglue = weight
         sinkhorn_iterations = 30
         match_threshold = match_threshold
         self.match_threshold = match_threshold
         config = {
-
             'superglue': {
                 'weights': superglue,
                 'sinkhorn_iterations': sinkhorn_iterations,
                 'match_threshold': match_threshold,
             }
         }
-        # self.superglue = SuperGlue(config.get('superglue', {})).eval().cuda()
+        # Initialize SuperGlue model with given configuration
         self.superglue = SuperGlue(config.get('superglue', {})).eval()
         self.superpoint = SuperPoint_api()
 
+    # Perform matching of keypoints between two images
     def matching(self, sup_data_first, img2_path, size=(768, 768)):
-
         refer = cv2.imread(img2_path, 1)
-
         refer = cv2.resize(refer, size)
-
         sup_inp = cv2.cvtColor(refer, cv2.COLOR_BGR2GRAY) / 255.
-
         sup_res_refer = self.superpoint.run_SuperPoint(sup_inp, name='1')
-
         data = {**sup_data_first, **sup_res_refer}
-
         for k in data:
             if isinstance(data[k], (list, tuple)):
                 data[k] = torch.stack(data[k])
-
-        # Perform the matching
         pred = {**data, **self.superglue(data)}
-
         return pred
 
+    # Register keypoints between two images and return the transformed image
     def registration(self, pred, jsr_res=None, show_matching=True):
+        # Converting tensors to numpy arrays for further processing
         pred = {k: v[0].cpu().numpy() for k, v in pred.items()}
         kpts0, kpts1 = pred['keypoints0'], pred['keypoints1']
         matches, conf = pred['matches0'], pred['matching_scores0']
 
+        # Select the keypoints with valid matches
         valid = matches > -1
         mkpts0 = kpts0[valid]
         mkpts1 = kpts1[matches[valid]]
         mconf = conf[valid]
 
+        # Filter the matched keypoints based on match threshold
         match_threshold = self.match_threshold
-
         mkpts0 = mkpts0[mconf > match_threshold]
         mkpts1 = mkpts1[mconf > match_threshold]
 
-        # Keep the matching keypoints to show.
-
-        # cv_kpts1 = [cv2.KeyPoint(int(i[0]), int(i[1]), 20)
-        #             for i in mkpts0]
-        # cv_kpts2 = [cv2.KeyPoint(int(i[0]), int(i[1]), 20)
-        #             for i in mkpts1]
-
+        # Create an array of matched points
         src_pts = np.float32(mkpts0).reshape(-1, 1, 2)
         dst_pts = np.float32(mkpts1).reshape(-1, 1, 2)
 
+        # Compute the homography matrix if enough matched points are present
         M = None
         if len(src_pts) > 4:
             M, mask = cv2.findHomography(src_pts, dst_pts, cv2.LMEDS, 1)
 
+        # Check if homography
+        # matrix has been computed successfully
         if M is None:
             print('Registration failed')
             return None
+
+        # Use homography to warp perspective
         align = cv2.warpPerspective(jsr_res, M, (jsr_res.shape[1], jsr_res.shape[0]), borderMode=cv2.BORDER_CONSTANT,
                                             borderValue=0)
-        # return align
-        # else:
+
+        # Show the matches between the two images if specified
         if show_matching:
             query = pred['image0'].squeeze()
             refer = pred['image1'].squeeze()
             query = (cv2.cvtColor(query, cv2.COLOR_GRAY2RGB)*255).astype(np.uint8)
             refer = (cv2.cvtColor(refer, cv2.COLOR_GRAY2RGB)*255).astype(np.uint8)
+
+            # Apply homography to the first image
             align_query = cv2.warpPerspective(query, M, (jsr_res.shape[1], jsr_res.shape[0]), borderMode=cv2.BORDER_CONSTANT,
                                             borderValue=0)
+
             color = cm.jet(mconf)
+
+            # Create matching plot
             show1 = make_matching_plot_fast(
                 query, refer, kpts0, kpts1, mkpts0, mkpts1, color,
                 'SuperGlue')
-            plt.imshow(show1)
-            plt.annotate('SUerGlue Results', (0, 0), color='red', fontsize=20)
-            plt.show()
-            io.savemat('SUperGlueResults.mat', {'show1': show1})
 
+            # Display matching plot
+            plt.imshow(show1)
+            plt.annotate('SuperGlue Results', (0, 0), color='red', fontsize=20)
+            plt.show()
+
+            # Save matching plot
+            io.savemat('SuperGlueResults.mat', {'show1': show1})
+
+            # Display original and transformed first image, and the reference image
             plt.imshow(query)
-            plt.annotate('First GRay', (0, 0), color='red', fontsize=20)
+            plt.annotate('First Gray', (0, 0), color='red', fontsize=20)
             plt.show()
 
             show_align = blend(align_query, refer)
             qtensor = torch.from_numpy(align_query).permute(2, 0, 1)[None, ...] / 255.
             rtensor = torch.from_numpy(refer).permute(2, 0, 1)[None, ...] / 255.
 
-
             plt.imshow(show_align)
             plt.annotate('Matching Results', (0, 0), color='red', fontsize=20)
             plt.show()
+
+            # Save matching results
             io.savemat('MatchingResults.mat', {'show_align': show_align})
 
             plt.imshow(align_query)
             plt.annotate('align_query', (0, 0), color='red', fontsize=20)
             plt.show()
+
+            # Save warped first image
             io.savemat('align_query.mat', {'align_query': align_query})
 
             plt.imshow(refer)
             plt.annotate('refer', (0, 0), color='red', fontsize=20)
             plt.show()
+
+            # Save reference image
             io.savemat('refer.mat', {'refer': refer})
 
         return align
+
